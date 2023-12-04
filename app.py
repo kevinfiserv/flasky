9efdd1794d6d6d20c8d4fc9fefdef81d4c014984
Kevin Ramos <kevin.ramos@fiserv.com>
2023-12-04 10:31:14 -0500

    get & delete items from db

--- app.py
+++ app.py
@@ -39,7 +39,62 @@ def addTodo():
     mysql.connection.commit()
     cursor.close()
 
-    return f"0{item}"
+    return f"added {item}"
+
+
+@app.route("/removeOneTodo", methods=["POST"])
+def removeOneTodo():
+    request_data = request.get_json()
+    item_id = request_data["id"]
+
+    cursor = mysql.connection.cursor()
+    command = f""" delete from todolist where id={item_id} """
+
+    cursor.execute(command)
+    mysql.connection.commit()
+    cursor.close()
+
+    return f"deleted {item_id}"
+
+
+@app.route("/getLastId", methods=["GET"])
+def getLastId():
+    cursor = mysql.connection.cursor()
+    command = f""" SELECT * FROM todolist ORDER BY ID DESC LIMIT 1 """
+
+    cursor.execute(command)
+    data = cursor.fetchall()
+    cursor.close()
+
+    try:
+        item_id = jsonify(data[0][0])
+    except:
+        item_id = jsonify(1)
+
+    return item_id
+
+
+@app.route("/reset_the_DB", methods=["PUT"])
+def resetDB():
+    cursor = mysql.connection.cursor()
+    command = f"""
+
+    drop table todolist;
+    
+    create table  todolist (
+    id int AUTO_INCREMENT,
+	item varchar(255) ,
+	createdDate DATETIME DEFAULT now(),
+	primary key (id)
+    );
+
+    
+    """
+
+    cursor.execute(command)
+    cursor.close()
+
+    return jsonify({"Response": "Complete"})
 
 
 app.run(debug=True)
--- static/app.js
+++ static/app.js
@@ -1,11 +1,15 @@
 // add task functionality
-function addTask() {
+async function addTask() {
   // get the input field and task list elements
+
+  const last_id = await getLastId();
+
   let taskInput = document.getElementById("taskInput");
   let taskList = document.getElementById("taskList");
 
   let divEl = document.createElement("div");
   divEl.className = "outside";
+  divEl.id = last_id;
 
   let button = document.createElement("button");
   button.className = "button";
@@ -32,6 +36,7 @@ function addTask() {
     divEl.appendChild(button);
     button.onclick = function () {
       taskList.removeChild(divEl);
+      deleteTask(divEl.id);
     };
 
     fetch(`http://127.0.0.1:5000/addtodo`, {
@@ -48,3 +53,68 @@ function addTask() {
 }
 
 // delete task functionality
+
+async function getAllTasks() {
+  const res = await fetch(`http://127.0.0.1:5000/alltodos`, {
+    method: "GET",
+  });
+  const alltasks = await res.json();
+  // console.log(alltasks);
+
+  alltasks.forEach((element) => {
+    let divEl = document.createElement("div");
+    divEl.className = "outside";
+    divEl.id = element[0];
+
+    let button = document.createElement("button");
+    button.className = "button";
+    button.appendChild(document.createTextNode("Delete"));
+
+    let divForButton = document.createElement("div");
+    divForButton.className = "hoverEffect";
+    let blankDiv = document.createElement("div");
+
+    button.appendChild(divForButton);
+    button.appendChild(blankDiv);
+    let li = document.createElement("li");
+
+    // add the task text to the list item
+    li.appendChild(document.createTextNode(element[1]));
+
+    // display the list item to the task list
+    taskList.appendChild(divEl);
+    divEl.appendChild(li);
+    divEl.appendChild(button);
+    button.onclick = function () {
+      taskList.removeChild(divEl);
+      deleteTask(element[0]);
+    };
+  });
+
+  return alltasks;
+}
+
+async function deleteTask(id_param) {
+  const res = await fetch(`http://127.0.0.1:5000/removeOneTodo`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id: String(id_param) }),
+  });
+  return res;
+}
+
+async function getLastId() {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  const item_id = await fetch("http://127.0.0.1:5000/getLastId", requestOptions)
+    .then((response) => response.text())
+    // .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+
+  return item_id;
+}
